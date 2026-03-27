d89823e6df0aec018eb4172539fab23f93d328c4
Monish C <86337147+Monish0401@users.noreply.github.com>
2026-03-27 13:32:20 +0530

    Add Header and Footer components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Toaster } from 'sonner';
 import { Canvas } from './components/Canvas';
+import { Header } from './components/ui/Header';
+import { Footer } from './components/ui/Footer';
 import './styles/canvas.css';
 
 export default function App() {
   return (
     <>
+      <Header />
       <Canvas />
       <Toaster position="bottom-right" />
+      <Footer />
     </>
   );
 }
