180581ca902224ac8461b1d11b65f097af60ecf4
Monish C <86337147+Monish0401@users.noreply.github.com>
2026-03-27 13:45:33 +0530

    Error solve in Header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from 'sonner';
 import { Canvas } from './components/Canvas';
-import { Header } from './components/ui/Header';
-import { Footer } from './components/ui/Footer';
+import  Header  from './components/ui/Header';
+import  Footer  from './components/ui/Footer';
 import './styles/canvas.css';
 
 export default function App() {
